7ca15b02519130389d82b5c62bd1d854a20948ed
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2020-12-06 18:08:19 -0600

    Test: - Create events table and added assosiations with User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates_presence_of :name
+  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_203638) do
+ActiveRecord::Schema.define(version: 2020_12_06_233951) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "event_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
