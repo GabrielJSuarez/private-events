30dbe594817000f5fefa65b4e23a5b03efd6e6c2
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2020-12-07 10:25:25 -0600

    Feature:
    - Add root page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -17,10 +17,10 @@ module UsersHelper
     user.created_events.each do |event|
       concat(
         content_tag(:div, nil, class: 'card my-4') do
-          content_tag(:div, "#{user.name.capitalize} Event", class: 'card-header' ) +
+          content_tag(:div, "#{user.name.capitalize} Event", class: 'card-header') +
             content_tag(:div, nil, class: 'card-body') do
               content_tag(:h5, event.event_name.to_s, class: 'card-title') +
-                content_tag(:p, 'Event description placeholder', class: 'card-text')
+                content_tag(:p, event.description.to_s, class: 'card-text')
             end +
             content_tag(:div, "#{time_ago_in_words(event.created_at)} ago", class: 'card-footer text-muted')
         end
