d18ce63ed6d1f60c96027e63a8b95804f2977e37
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2020-12-08 12:29:18 -0600

    Feature - Add style to landing page and it's helper method

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,10 @@
 module PagesHelper
+  def home_buttons
+    if logged_in?
+      (link_to 'Log Out', logout_path, method: :delete, data: { confirm: 'Are you sure you want to log out' }, class: 'btn btn-danger')
+    else
+      (link_to 'Log in', login_path, class: 'btn btn-primary mr-3') +
+        (link_to 'Sign Up', new_user_path, class: 'btn btn-success')
+    end
+  end
 end
